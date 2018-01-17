ddb62eb027efa7b8609904918ba4561da09be5e6
Ryan <32820153+AvocadoFire@users.noreply.github.com>
2018-01-17 00:44:04 +0000

    Update Exercise 9 - Guessing Game One.py

--- Exercise 9 - Guessing Game One.py
+++ Exercise 9 - Guessing Game One.py
@@ -2,13 +2,16 @@
 # 2. Ask the user to guess the number, then tell them if they are low, high or right
 
 import random
-print("Guess a number between 1 and 9 or type 'exit'\n")
+print("Guess a number between 1 and 9 or type 0 to exit\n")
 a = random.randint(1,9)
 guesscount = 1
+
 while True:
     guess = int(input("Guess: "))
     if guess == a:
         print("You got it in",guesscount,"!")
+        break
+    elif guess == 0:
         break
     elif guess < a:
         print("Too low")
@@ -16,17 +19,3 @@ while True:
     elif guess > a:
         print ("Too high")
         guesscount += 1
-
-        
-        
-        
-
-
-  
-    
-
-
-
-        
-
-
