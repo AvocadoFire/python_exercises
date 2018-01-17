88db4ca9536e99fe036db8ed64835b12affd5e30
Ryan <32820153+AvocadoFire@users.noreply.github.com>
2018-01-17 00:33:11 +0000

    Add files via upload

---
+++ Exercise 9 - Guessing Game One.py
@@ -0,0 +1,32 @@
+# 1. Generate a random number between 1 and 9.
+# 2. Ask the user to guess the number, then tell them if they are low, high or right
+
+import random
+print("Guess a number between 1 and 9 or type 'exit'\n")
+a = random.randint(1,9)
+guesscount = 1
+while True:
+    guess = int(input("Guess: "))
+    if guess == a:
+        print("You got it in",guesscount,"!")
+        break
+    elif guess < a:
+        print("Too low")
+        guesscount += 1
+    elif guess > a:
+        print ("Too high")
+        guesscount += 1
+
+        
+        
+        
+
+
+  
+    
+
+
+
+        
+
+
